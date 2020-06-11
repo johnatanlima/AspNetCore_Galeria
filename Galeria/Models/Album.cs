2464640f7cdd0ea5ec176d38a11bae70dfdd0ee3
Johnatan Lima <johw.23@outlook.com>
2020-06-11 17:44:32 -0300

    Alteração de projeto, design, criação de novos modelos.

---
+++ Galeria/Models/Album.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Galeria.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+
+        [Required(ErrorMessage ="Campo obrigatório.")]
+        [StringLength(64, ErrorMessage ="Limite de caracteres excedido.")]
+        public string Destino { get; set; }
+
+        [Required(ErrorMessage ="Campo obrigatório.")]
+        public string FotoTopo { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/mm/yyyy}")]
+        public DateTime Inicio { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/mm/yyyy}")]
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        [DataType(DataType.Date)]
+        public DateTime Fim { get; set; }
+    }
+}
