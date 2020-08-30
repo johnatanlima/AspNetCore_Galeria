944e57688964878611c73b880a7cd4c8d65c07b8
Johnatan Lima <johw.23@outlook.com>
2020-08-30 18:55:17 -0300

    Criação do controller pra adicionar imagens aos albuns

--- Galeria/Controllers/ImagensController.cs
+++ Galeria/Controllers/ImagensController.cs
@@ -8,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using Galeria.Data;
 using Galeria.Models;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace Galeria.Controllers
 {
@@ -22,22 +24,34 @@ namespace Galeria.Controllers
             _env = env;
         }
       
-        public IActionResult Create()
+        public IActionResult Create(int id)
         {
-            ViewData["AlbumId"] = new SelectList(_context.Albuns, "AlbumId", "Destino");
+
+            ViewBag.Destinos = _context.Albuns.FirstOrDefault(x => x.AlbumId == id);
+
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ImagemId,LinkImagem,AlbumId")] Imagem imagem)
+        public async Task<IActionResult> Create([Bind("ImagemId,LinkImagem,AlbumId")] Imagem imagem, IFormFile files)
         {
             if (ModelState.IsValid)
             {
+                //Lógica => Monto um caminho na aplicação, concateno com o nome do arquivo que recebo, crio o arquivo com filestream no diretorio, copio o arquivo recebido no filestream, recupero caminho do arquivo inteiro, armazeno na propriedade do objeto.
+                var caminhoFoto = Path.Combine(_env.ContentRootPath, "wwwroot/Imagens"); //Concatena o caminho do diretório Imagens, através do método combine, que recebe o caminho do diretório em que aplicação está sendo executada.
+
+                using (var fs = new FileStream(Path.Combine(caminhoFoto, files.FileName), FileMode.Create)) //uso o caminho do diretório, e concateno com o nome da imagem que está vindo em minha requisição
+                {
+                    await files.CopyToAsync(fs); //copio a imagem da requisão para o arquivo filestream que criei no diretório logo acima.
+                    imagem.LinkImagem = "~/Imagens/" + files.FileName;
+                }
+
                 _context.Add(imagem);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Details", "Albuns", new {id = imagem.AlbumId});
             }
+           
             ViewData["AlbumId"] = new SelectList(_context.Albuns, "AlbumId", "Destino", imagem.AlbumId);
             return View(imagem);
         }
